0b11f3957169055be6c20b2cec8c8baae27d80b9
KH-RIPPER <khripper2003@gmail.com>
2024-09-23 23:55:46 +0100

    test

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,20 @@ import Cookies from "js-cookie";
 
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    Authorization: `bearer ${Cookies.get("token")}`,
-  },
 });
+
+// Interceptor to add the Authorization header
+API.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default API;
